5fc9c717dd0fc1f0a91c12ebf03aa422edb90429
Siva <doodsiva@gmail.com>
2022-06-17 20:34:14 +0530

    create passenger class containing passenger details

---
+++ Passenger.java
@@ -0,0 +1,16 @@
+public class Passenger
+{
+    String name;
+    int age;
+    String preferredbirth;
+
+    String allotedBirth;
+    int allotedSeat;
+    
+    public Passenger (String name, int age, String preferredbirth)
+    {
+        this.name = name;
+        this.age = age;
+        this.preferredbirth = preferredbirth;
+    }
+}
